e0482cfdc5c4ceaf846b7cfce16d4c550e7acd59
zhangwei <1112@163.com>
2018-11-27 15:43:34 +0800

    详情界面的参数,评论数

--- apps/detail/views.py
+++ apps/detail/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from apps.main.models import Shop, ShopImage
+from apps.main.models import Shop, ShopImage, PropertyValue, Review
 
 """
 ?
@@ -23,7 +23,16 @@ def detail(request):
                 shop = shops.first()
                 imgs = ShopImage.objects.filter(shop_id=shop.get('shop_id')).values('shop_img_id', 'type')
                 shop.update(imgs=imgs)
-                return render(request, 'detail.html', {'shop': shop})
+                values = PropertyValue.objects.filter(shop_id=shop_id)
+                reviews = Review.objects.filter(shop_id=shop_id)
+                return render(request, 'detail.html', {'shop': shop, 'values': values, 'reviews': reviews})
+            # 一对一查询
+            # 正向查询反向查询 都能查询出相关的子表数据
+            #  user.userprofile.phone
+            # userprofile.user.username
+            # 一对多 查询
+            #  正向查询 能查询出主表的相关数据
+            #  反向查询
         except Exception as e:
             print(e)
     else:
